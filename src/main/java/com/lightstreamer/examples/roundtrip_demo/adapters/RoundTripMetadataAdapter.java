b1d97a02a7b1a2dfad7c30058ff4352d9237a9d1
Dario Crivelli <dario.crivelli@lightstreamer.com>
2023-06-06 11:01:03 +0200

    Align to interface tweak.

--- src/main/java/com/lightstreamer/examples/roundtrip_demo/adapters/RoundTripMetadataAdapter.java
+++ src/main/java/com/lightstreamer/examples/roundtrip_demo/adapters/RoundTripMetadataAdapter.java
@@ -19,7 +19,6 @@ package com.lightstreamer.examples.roundtrip_demo.adapters;
 import java.io.File;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.logging.log4j.LogManager;
@@ -85,7 +84,7 @@ public class RoundTripMetadataAdapter extends LiteralBasedProvider {
      * Triggered by a client "sendMessage" call.
      * The message encodes a chat message from the client.
      */
-    public CompletionStage<String> notifyUserMessage(String user, String session, String message)
+    public CompletableFuture<String> notifyUserMessage(String user, String session, String message)
         throws NotificationException, CreditsException {
 
         // we won't introduce blocking operations, hence we can proceed inline
